473843329f855fc80e1a7905d706fd52f9370349
wilpola <wilpolaproduction@gmail.com>
2025-06-19 09:42:19 +0300

    Add layout

---
+++ part-2/countries/src/Layout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="w-full bg-gray-100 h-screen">
+      <header className="h-14 bg-neutral-800 text-white ">
+        <div className="max-w-screen-lg mx-auto w-[95%] flex items-center justify-between h-full">
+          <h1 className="text-2xl font-semibold">Countries</h1>
+          <div className="">
+            <Button
+              variant="ghost"
+              className=" hover:bg-neutral-700 text-white hover:text-white"
+            >
+              Home
+            </Button>
+            <Button
+              variant="ghost"
+              className=" hover:bg-neutral-700 text-white hover:text-white"
+            >
+              About
+            </Button>
+          </div>
+        </div>
+      </header>
+      <main className="h-[calc(100vh-56px)]">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
