65af550d5954177ef3412ffd212d5297a9f537a0
wilpola <wilpolaproduction@gmail.com>
2025-06-19 13:00:15 +0300

    Add country types

---
+++ part-2/countries/src/views/Home/CountryTypes.ts
@@ -0,0 +1,85 @@
+
+export interface ICountry {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: {
+    [key: string]: string;
+  };
+  translations: {
+    [key: string]: {
+      official: string;
+      common: string;
+    };
+  };
+  latlng: number[];
+  landlocked: boolean;
+  borders: string[];
+  area: number;
+  demonyms: {
+    [key: string]: {
+      f: string;
+      m: string;
+    };
+  };
+  flag: string;
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+    tld: string[];
+  };
+  population: number;
+  fifa: string;
+  car: {
+    signs: string[];
+    side: string;
+  };
+  timezones: string[];
+  continents: string[];
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  coatOfArms: {
+    png: string;
+    svg: string;
+  };
+  startOfWeek: string;
+  capitalInfo: {
+    latlng: number[];
+  };
+  postalCode: {
+    format: string;
+    regex: string;
+  };
+}
