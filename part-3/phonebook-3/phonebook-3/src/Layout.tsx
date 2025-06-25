2bf56fdc958802ecd6938e9e3741c0c2c7d71955
wilpola <wilpolaproduction@gmail.com>
2025-06-25 20:47:01 +0300

    Add new layout

--- part-3/phonebook-3/phonebook-3/src/Layout.tsx
+++ part-3/phonebook-3/phonebook-3/src/Layout.tsx
@@ -1,12 +1,52 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
+import { Button } from "./components/ui/button";
+import { NavLink as Link } from "react-router-dom";
+import { Github } from "lucide-react";
 
 const Layout: React.FC = () => {
   return (
     <>
+      <div className="w-full bg-gray-100 h-screen">
+        <header className="h-14 bg-neutral-800 text-white ">
+          <div className="max-w-screen-lg mx-auto w-[95%] flex items-center justify-between h-full">
+            <h1 className="text-2xl font-semibold">Phonebook-v3</h1>
+            <div className="flex items-center">
+              <Button
+                variant="ghost"
+                className=" hover:bg-neutral-700 text-white hover:text-white"
+                asChild
+              >
+                <Link to="/">Home</Link>
+              </Button>
+              <Button
+                variant="ghost"
+                className=" hover:bg-neutral-700 text-white hover:text-white"
+                asChild
+              >
+                <Link to="/about">About</Link>
+              </Button>
+              <Button
+                variant="ghost"
+                className=" hover:bg-neutral-700 text-white hover:text-white"
+                size={"icon"}
+                onClick={() =>
+                  window.open(
+                    "https://github.com/wilpola/Mooc2024/tree/main/part-3"
+                  )
+                }
+              >
+                <Github />
+              </Button>
+            </div>
+          </div>
+        </header>
+        <main className="h-[calc(100vh-56px)] overflow-y-scroll">
+          <Outlet />
+        </main>
+      </div>
       <Toaster />
-      <Outlet />
     </>
   );
 };
