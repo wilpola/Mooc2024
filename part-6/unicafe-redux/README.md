6abf1df77e5a70d0ce60a9b38506fc946ca00e60
wilpola <wilpolaproduction@gmail.com>
2025-09-22 21:33:54 +0300

    Initialize part-6.1

---
+++ part-6/unicafe-redux/README.md
@@ -0,0 +1,3 @@
+# Part-6 | Learning Redux
+
+In this section we learn state management, context, reducers and more.
---
+++ part-6/unicafe-redux/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Part 6 | Redux</h1>
+      <p>Welcome to Redux</p>
+    </>
+  );
+}
+
+export default App;
